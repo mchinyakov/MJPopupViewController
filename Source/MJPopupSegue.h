42d31a75642ece81025ad75f2186eeb7eb18ba7b
Mikhail Chinyakov <m.chinyakov@m-st.ru>
2014-01-18 14:35:19 +0400

    Create segue.

---
+++ Source/MJPopupSegue.h
@@ -0,0 +1,17 @@
+//
+//  MJPopupSegue.h
+//  MJPopupViewControllerDemo
+//
+//  Created by Mikhail Chinyakov on 18.01.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UIViewController+MJPopupViewController.h"
+
+@interface MJPopupSegue : UIStoryboardSegue
+
+@property (readwrite) MJPopupViewAnimation animation;
+
+@end
---
+++ Source/MJPopupSegue.m
@@ -0,0 +1,31 @@
+//
+//  MJPopupSegue.m
+//  MJPopupViewControllerDemo
+//
+//  Created by Mikhail Chinyakov on 18.01.14.
+//
+//
+
+#import "MJPopupSegue.h"
+
+#import "MJPopupBackgroundView.h"
+#import "UIViewController+MJPopupViewController.h"
+
+@implementation MJPopupSegue
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.animation = MJPopupViewAnimationFade;
+    }
+    return self;
+}
+
+- (void)perform
+{
+    [self.sourceViewController presentPopupViewController:self.destinationViewController
+                                            animationType:self.animation];
+}
+
+@end
